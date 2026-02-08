aab5684625f2b23b7db2225330ef38c894e9a7e5
yolobanana <62411419+yolobanana@users.noreply.github.com>
2026-02-08 09:57:43 +0700

    feat: add vercel analytics (#12)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { APP_NAME, SERVER_URL } from "@/lib/constants";
@@ -41,6 +42,7 @@ export default function RootLayout({
                 >
                     {children}
                 </ThemeProvider>
+                <Analytics />
             </body>
         </html>
     );
